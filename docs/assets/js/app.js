8b5fce958eab0b5acd05b527513c1ee03ac947a2
manodecristiano <cristian.carre.online@gmail.com>
2022-06-29 01:00:08 +0200

    Some changes

---
+++ docs/assets/js/app.js
@@ -0,0 +1,12 @@
+import * as toast from 'bootstrap';
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
